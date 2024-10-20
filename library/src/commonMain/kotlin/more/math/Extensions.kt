457b7f6914428fb926c6fba089eef1190414b4b1
Slava <83229808+plumsoftware@users.noreply.github.com>
2024-10-21 00:28:04 +0600

    Develop v0.0.3 (#3)

--- library/src/commonMain/kotlin/more/math/Extensions.kt
+++ library/src/commonMain/kotlin/more/math/Extensions.kt
@@ -3,10 +3,10 @@ package more.math
 import more.math.matrix.model.Matrix
 import more.math.vector.model.Vector2
 
-inline fun <reified R> matrixOf(vararg row: List<Any>): Matrix<R> {
+public inline fun <reified R> matrixOf(vararg row: List<Any>): Matrix<R> {
     return Matrix(*row.map { it.map { elem -> elem as R } }.toTypedArray())
 }
 
-inline fun <reified T : Number> vector2Of(x: T, y: T): Vector2<T> {
+public inline fun <reified T : Number> vector2Of(x: T, y: T): Vector2<T> {
     return Vector2(x, y)
 }
