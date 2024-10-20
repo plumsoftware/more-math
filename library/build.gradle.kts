eba6170fd19dede3cc5a565111eb09beca9df909
Slava <plumsoftwareofficial@gmail.com>
2024-10-20 23:20:17 +0600

    Gradle [~] Подредачил данные в градл.

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,11 +1,11 @@
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
-    id("module.publication")
-    id("com.vanniktech.maven.publish") version "0.29.0"
+    alias(libs.plugins.vanniktech.mavenPublish)
 }
 
 kotlin {
@@ -43,5 +43,39 @@ android {
     compileSdk = libs.versions.android.compileSdk.get().toInt()
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    coordinates(group.toString(), "more-math", version.toString())
+
+    pom {
+        name.set("More math")
+        description.set("Это KMP библиотека слегка упрощающая работу с математикой.")
+        url.set("https://github.com/plumsoftware/more-math")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+        developers {
+            developer {
+                id.set("https://t.me/Sl1vka")
+                name.set("Vyacheslav Deych")
+                organization.set("Plumsoftware")
+                organizationUrl.set("https://github.com/plumsoftware")
+            }
+        }
+        scm {
+            url.set("https://github.com/plumsoftware/more-math")
+            connection.set("scm:git:git://github.com/plumsoftware/more-math")
+            developerConnection = "scm:git:ssh://git@github.com:plumsoftware/more-math.git"
+        }
     }
 }
