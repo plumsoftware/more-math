3e5f026728e167ff87610a39bafd02628c47f717
Slava <plumsoftwareofficial@gmail.com>
2024-11-19 14:05:31 +0600

    Matrix [~] Пофиксил тесты для матриц

--- library/src/androidUnitTest/kotlin/more.math/MoreMathTest.kt
+++ library/src/androidUnitTest/kotlin/more.math/MoreMathTest.kt
@@ -12,8 +12,8 @@ class MoreMathTest {
     @Test
     fun test() {
         val matrix = matrixOf<Int>(
-            listOf(1,2),
-            listOf(3,4),
+            mutableListOf(1,2),
+            mutableListOf(3,4),
         )
         val vector1 = vector2Of(1, 2)
         val vector2 = vector2Of(3, 4)
@@ -21,7 +21,7 @@ class MoreMathTest {
         assertEquals(expected = -2.0, actual = matrix.determinant())
         assertEquals(expected = 6, MoreMath.factorial(3))
         assertEquals(expected = 2.0, MoreMath.average(1, 2, 3))
-        assertEquals(expected = Matrix(listOf(1, 3), listOf(2, 4)), matrix.transpose())
+        assertEquals(expected = Matrix(mutableListOf(1, 3), mutableListOf(2, 4)), matrix.transpose())
         assertEquals(expected = 16.0, MoreMath.tetraction(2.0, 3))
         assertEquals(expected = Vector2(x = 4, y = 6), actual = (vector1 + vector2))
     }
